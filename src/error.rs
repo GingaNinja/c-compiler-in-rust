0d9f8e36a66d5d0d326cf1199c817b598d4d11cd
Dave Arkell <dave.arkell@dae.mn>
2026-05-30 16:18:07 +0100

    added my missing error.rs

---
+++ src/error.rs
@@ -0,0 +1,63 @@
+use crate::lex::{Keyword, Token};
+
+pub enum DccError {
+    PreProcess,
+    ExeCreate,
+    ExtraTokens,
+    ExpectedToken { actual: Token, expected: String },
+    ExpectedKeyword { actual: Token, expected: Keyword },
+    ExpectedMoreTokens { expected: String },
+    ExpectedMoreKeywords { expected: Keyword },
+    InvalidInputChar { pos: usize, found_char: String },
+}
+
+impl std::fmt::Debug for DccError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_string())
+        // match self {
+        //     Self::PreProcess => write!(f, "issue running pre-process"),
+        //     Self::ExeCreate => write!(f, "issue creating the exe"),
+        //     Self::ExtraTokens => write!(f, "extra tokens at the end of the file"),
+        //     Self::ExpectedToken { actual, expected } => {
+        //         write!(f, "Expected '{expected}' but found '{actual}'")
+        //     }
+        //     Self::ExpectedMoreTokens { expected } => {
+        //         write!(f, "Expected '{expected}' but reached the end")
+        //     }
+        //     Self::InvalidInputChar { pos, found_char } => {
+        //         write!(f, "invalid input, char {pos} - {found_char}")
+        //     }
+        //     Self::ExpectedKeyword { actual, expected } => {
+        //         write!(f, "Expected '{expected}' but found '{actual}'")
+        //     }
+        //     Self::ExpectedMoreKeywords { expected } => {
+        //         write!(f, "Expected '{expected}' but reached the end")
+        //     }
+        // }
+    }
+}
+
+impl std::fmt::Display for DccError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::PreProcess => write!(f, "issue running pre-process"),
+            Self::ExeCreate => write!(f, "issue creating the exe"),
+            Self::ExtraTokens => write!(f, "extra tokens at the end of the file"),
+            Self::ExpectedToken { actual, expected } => {
+                write!(f, "Expected '{expected}' but found '{actual}'")
+            }
+            Self::ExpectedMoreTokens { expected } => {
+                write!(f, "Expected '{expected}' but reached the end")
+            }
+            Self::InvalidInputChar { pos, found_char } => {
+                write!(f, "invalid input, char {pos} - {found_char}")
+            }
+            Self::ExpectedKeyword { actual, expected } => {
+                write!(f, "Expected '{expected}' but found '{actual}'")
+            }
+            Self::ExpectedMoreKeywords { expected } => {
+                write!(f, "Expected '{expected}' but reached the end")
+            }
+        }
+    }
+}
